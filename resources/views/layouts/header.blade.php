9a4c00c42f11c8a701fab99e32bb6708c78d1102
samuel mwangi <samuelmwangi729@gmail.com>
2020-05-06 17:53:30 +0300

    final

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Amin Template">
-    <meta name="keywords" content="Amin, unica, creative, html">
+    <meta name="description" content="Video Streaming Service">
+    <meta name="keywords" content="Video, Youtube,Streaming,Categories,Google Videos,Earn videos,index of Videos">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }} | HomePage</title>
