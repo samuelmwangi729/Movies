3a7f16d030a655194ee610fb3fd1fc3f0add9a96
samuel mwangi <samuelmwangi729@gmail.com>
2020-05-08 12:53:56 +0300

    final

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -14,7 +14,7 @@ class PaymentController extends Controller
         Twocheckout::privateKey('5EE6AC35-CC52-4714-A3A3-B810EA1B2DEF'); //Private Key
         Twocheckout::sellerId('250350404436'); // 2Checkout Account Number
         Twocheckout::username('demotestuser');
-        Twocheckout::password('P!@#four5sam');
+        Twocheckout::password('demotestuser');
         Twocheckout::sandbox(false); // Set to false for production accounts.
 
 
