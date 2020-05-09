6446a5e7b68c5ce7fb1eccad0924f8cf35c31e77
samuel mwangi <samuelmwangi729@gmail.com>
2020-05-09 14:04:59 +0300

    final

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,81 +1,55 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Payments\Twocheckout;
-use App\Http\Controllers\Payments\Twocheckout_Charge;
-use App\Http\Controllers\Payments\Twocheckout\Api\Twocheckout_Error;
 use Illuminate\Http\Request;
 use Auth;
 use App\CatSubscriber;
 use Session;
+use Stripe\Stripe;
+use Stripe\Charge;
 class PaymentController extends Controller
 {
     public function index(Request $request){
-        Twocheckout::privateKey('5EE6AC35-CC52-4714-A3A3-B810EA1B2DEF'); //Private Key
-        Twocheckout::sellerId('250350404436'); // 2Checkout Account Number
-        Twocheckout::username('vildstream');
-        Twocheckout::password('P1@3four5sam');
-        Twocheckout::sandbox(false); // Set to false for production accounts.
+        Stripe::setApiKey('sk_test_YrU8aAxLo4HFb1P9HkOAd8Pd00oL1pZIMa');
 
+        $customer = \Stripe\Customer::create([
+            'email' =>Auth::user()->email,
+            'source' => request()->stripeToken,
+          ]);
 
-        //end class
-        try {
-            $charge = Twocheckout_Charge::auth(array(
-                "merchantOrderId" => "123",
-                "token"      => $request->token,
-                "currency"   => 'USD',
-                "total"      => '1',
-                "billingAddr" => array(
-                    "name" => Auth::user()->name,
-                    "addrLine1" => $request->addrLine1,
-                    "city" => $request->city,
-                    "state" => $request->state,
-                    "zipCode" => $request->zipCode,
-                    "country" => $request->country,
-                    "email" => Auth::user()->email,
-                    "phoneNumber" => $request->phone
-                )
-            ));
-
-            if ($charge['response']['responseCode'] == 'APPROVED') {
-                // dd($charge);
-                // echo "Thanks for your Order!";
-                // echo "<h3>Return Parameters:</h3>";
-                // echo "<pre>";
-                // print_r($charge);
-                // echo "</pre>";
-                $isExist=CatSubscriber::where([
-                    ['Subscriber','=',Auth::user()->email],
-                    ['Category','=',$request->category]
-                ])->get()->first();
-                if(is_null($isExist) || $isExist->count()==0){
-                    $today=date('Y-m-d');
-                    $endDate=date('Y-m-d',strtotime($today. '+30 days'));
-                    CatSubscriber::create([
-                        'Subscriber'=>Auth::user()->email,
-                        'Category'=>$request->category,
-                        'EndDate'=>$endDate
-                    ]);
-                    Session::flash('success','Payments Successfully Received, You are now Susbcribed');
-                    return back();
-                }
-                if($isExist->Status==1){
-                    $today=date('Y-m-d');
-                    $endDate=date('Y-m-d',strtotime($today. '+30 days'));
-                    $isExist->Subscriber=Auth::user()->email;
-                    $isExist->category=$request->category;
-                    $isExist->EndDate=$endDate;
-                    $isExist->Status=0;
-                    $isExist->save();
-                    Session::flash('success','Subscription Successfully Renewed');
-                    return back();
-                }
-
-
+          $charge = \Stripe\Charge::create([
+            'customer' => $customer->id,
+            'description' => 'Member Payments to Vildstream',
+            'amount' => 1 * 100 * 76,
+            'currency' => 'inr',
+          ]);
+          if($charge->status=="succeeded"){
+            $isExist=CatSubscriber::where([
+                ['Subscriber','=',Auth::user()->email],
+                ['Category','=',$request->category]
+            ])->get()->first();
+            if(is_null($isExist) || $isExist->count()==0){
+                $today=date('Y-m-d');
+                $endDate=date('Y-m-d',strtotime($today. '+30 days'));
+                CatSubscriber::create([
+                    'Subscriber'=>Auth::user()->email,
+                    'Category'=>$request->category,
+                    'EndDate'=>$endDate
+                ]);
+                Session::flash('success','Payments Successfully Received, You are now Susbcribed');
+                return back();
             }
-        }catch (Twocheckout_Error $e) {
-            Session::flash('error',$e->getMessage());
-            return back();
-        }
+            if($isExist->Status==1){
+                $today=date('Y-m-d');
+                $endDate=date('Y-m-d',strtotime($today. '+30 days'));
+                $isExist->Subscriber=Auth::user()->email;
+                $isExist->category=$request->category;
+                $isExist->EndDate=$endDate;
+                $isExist->Status=0;
+                $isExist->save();
+                Session::flash('success','Subscription Successfully Renewed for 30 Days');
+                return back();
+            }
+          }
     }
 }
