4046859eb1f86da5ff89b7cc6aed3ade7e7dc3e9
samuel mwangi <samuelmwangi729@gmail.com>
2020-06-05 17:55:23 +0300

    final

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -61,7 +61,7 @@
                             <a href="#"><i class="fa fa-twitter"></i></a>
                             <a href="#"><i class="fa fa-youtube-play"></i></a>
                             <a href="#"><i class="fa fa-instagram"></i></a>
-                            <a href="#"><i class="fa fa-envelope"></i>&nbsp;Nitin54321@gmail.com</a>
+                            <a href="#"><i class="fa fa-envelope"></i>&nbsp;Nitinr54321@gmail.com</a>
                         </div>
                     </div>
                 </div>
